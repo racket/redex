racket/redex

commit 26e79b018a1bbafe24bd80df780d89c963c4e122

improve the contract on build-lw

redex-lib/redex/private/loc-wrapper.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (require racket/contract
+         pict
          (for-syntax racket/base)
          (for-syntax "loc-wrapper-ct.rkt")
          "loc-wrapper-rt.rkt")
@@ -24,7 +25,11 @@
              (column-span pnum)
              (unq? boolean?)
              (metafunction? boolean?)))
- [build-lw (-> any/c pnum pnum pnum pnum lw?)])
+ [build-lw (-> (or/c string?
+                     symbol?
+                     pict?
+                     (listof (or/c 'spring lw?)))
+               pnum pnum pnum pnum lw?)])
 
 (provide to-lw
          to-lw/uq

redex-doc/redex/scribblings/ref/typesetting.scrbl
@@ -1134,7 +1134,7 @@ metafunction application. See @racket[to-lw] for the meanings of the other field
 @defproc[(build-lw [e (or/c string?
                             symbol?
                             pict? 
-                            (listof (or/c (symbols 'spring) lw?)))]
+                            (listof (or/c 'spring lw?)))]
                    [line exact-positive-integer?]
                    [line-span exact-positive-integer?]
                    [column exact-positive-integer?]

commit a777ad7127e528d4d59acc641ec53407fdc92503

fix depenency problem introduced by 26e79b018a1

redex-lib/redex/private/loc-wrapper.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 
 (require racket/contract
-         pict
          (for-syntax racket/base)
          (for-syntax "loc-wrapper-ct.rkt")
          "loc-wrapper-rt.rkt")
@@ -24,13 +23,9 @@
              (column pnum)
              (column-span pnum)
              (unq? boolean?)
-             (metafunction? boolean?)))
- [build-lw (-> (or/c string?
-                     symbol?
-                     pict?
-                     (listof (or/c 'spring lw?)))
-               pnum pnum pnum pnum lw?)])
+             (metafunction? boolean?))))
 
 (provide to-lw
          to-lw/uq
-         curly-quotes-for-strings)
+         curly-quotes-for-strings
+         build-lw)

redex-pict-lib/redex/pict.rkt
@@ -136,7 +136,6 @@
  [current-render-pict-adjust (parameter/c (pict? symbol? . -> . pict?))])
 
 (provide
- build-lw
  lw
  lw?
  lw-e
@@ -149,7 +148,15 @@
          to-lw/stx
          (struct-out lw))
 
+(define pnum (and/c number? (or/c zero? positive?)))
+
 (provide/contract
+ [build-lw
+  (-> (or/c string?
+            symbol?
+            pict?
+            (listof (or/c 'spring lw?)))
+      pnum pnum pnum pnum lw?)]
  [just-before (-> (or/c pict? string? symbol?) lw? lw?)]
  [just-after (-> (or/c pict? string? symbol?) lw? lw?)]
  [fill-between (-> (or/c pict? string? symbol?) lw? lw? lw?)])