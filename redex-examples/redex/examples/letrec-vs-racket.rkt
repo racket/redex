24d7f7d83cd2a9c33ea260061967b4a72f81c284
Robby Findler <robby@racket-lang.org>
2018-08-19 15:39:08 -0500

    bring letrec.rkt up to date with modern redex and racket and just generally clean things up
    inspired by the discussion in racket/racket#2232

---
+++ redex-examples/redex/examples/letrec-vs-racket.rkt
@@ -0,0 +1,96 @@
+#lang racket
+(require "letrec.rkt" redex/reduction-semantics)
+
+#|
+
+Tests to see if the model in letrec.rkt
+produces the same results as racket itself
+
+|#
+
+(require racket/runtime-path)
+(define ns (make-base-empty-namespace))
+(module all-the-stuff racket/base
+  (provide + - * set! = #%top
+           let letrec if begin
+           #%app λ void #%datum))
+(define-runtime-path letrec-vs-racket.rkt "letrec-vs-racket.rkt")
+(require (only-in (submod "." all-the-stuff))) ;; bind nothing
+(namespace-attach-module (current-namespace)
+                         `(submod (file ,(path->string letrec-vs-racket.rkt)) all-the-stuff)
+                         ns)
+(parameterize ([current-namespace ns])
+  (namespace-require `(submod (file ,(path->string letrec-vs-racket.rkt)) all-the-stuff)))
+
+
+(define (same-as-racket? t)
+  (define cleaned-up (clean-up t))
+  (define redex-result (redex-eval cleaned-up))
+  (define racket-result (racket-eval cleaned-up))
+  (define ans (equal? redex-result racket-result))
+  (unless ans
+    (printf "cleaned up:\n")
+    (pretty-write cleaned-up))
+  ans)
+
+(define v? (redex-match? lang v))
+(define lam? (redex-match? lang (λ (x ...) e)))
+(define (redex-eval prog)
+  (define result (result-of prog))
+  (cond
+    [(or (lam? result) (member result '(* - + =))) 'proc]
+    [(v? result) result]
+    [else 'error]))
+
+(define (racket-eval prog)
+  (with-handlers ([exn:fail? (λ (x) 'error)])
+    (define result (eval prog ns))
+    (match result
+      [(? procedure?) 'proc]
+      [(? void?) '(void)]
+      [_ result])))
+
+;; clean-up : any -> any
+;; removes forms that shouldn't be in the original program
+;; removes (most of) the free variables
+(define (clean-up s)
+  (define primitives '(+ = * -))
+  (let loop ([s s]
+             [bound '()])
+    (define (pick-a-var x prim-ok?)
+      (cond
+        [(member x bound) x]
+        [(or (null? bound) (zero? (random 10))) x]
+        [else
+         (when prim-ok? (set! bound (append primitives bound)))
+         (list-ref bound (random (length bound)))]))
+    (match s
+      [`(letrec ([,xs ,es] ...) ,e)
+       (define new-vars (append xs bound))
+       `(letrec (,@(for/list ([x (in-list xs)]
+                              [e (in-list es)])
+                     `[,x ,(loop e new-vars)]))
+          ,(loop e new-vars))]
+      [`(let ([,xs ,es] ...) ,e)
+       (define new-vars (append xs bound))
+       `(let (,@(for/list ([x (in-list xs)]
+                           [e (in-list es)])
+                  `[,x ,(loop e bound)]))
+          ,(loop e new-vars))]
+      [`(λ (,xs ...) ,e)
+       (define new-vars (append xs bound))
+       `(λ (,@xs) ,(loop e new-vars))]
+      [`(set! ,x ,e)  `(set! ,(pick-a-var x #f) ,(loop e bound))]
+      [`(if ,e1 ,e2 ,e3)  `(if ,(loop e1 bound) ,(loop e2 bound) ,(loop e3 bound))]
+      [`(begin ,es ...) `(begin ,@(for/list ([e (in-list es)])
+                                    (loop e bound)))]
+      [`(void) `(void)]
+      [`(,ef ,eas ...)  `(,(loop ef bound) ,@(for/list ([ea (in-list eas)])
+                                               (loop ea bound)))]
+      [(? symbol?) (pick-a-var s #t)]
+      [(? boolean?) s]
+      [(? number?) s])))
+  
+(module+ test
+  (redex-check surface-lang e
+               (same-as-racket? (term e))))
--- redex-examples/redex/examples/letrec.rkt
+++ redex-examples/redex/examples/letrec.rkt
@@ -1,237 +1,276 @@
 #lang racket
+(require redex)
 
 #|
 
-BUG: letrec & let are not handled properly by substitution
+A model of Racket's letrec
 
 |#
 
-(require redex "subst.rkt")
+(provide lang
+         surface-lang
+         result-of
+         run)
 
-(reduction-steps-cutoff 20)
-
-(define-language lang
-  (p ((store (x v) ...) e))
+(define-language surface-lang
   (e (set! x e)
-     (let ((x e)) e)
-     (letrec ((x e)) e)
+     (let ((x_!_ e) ...) e)
+     (letrec ((x_!_ e) ...) e)
      (if e e e)
      (begin e e ...)
-     (e e)
+     (e e ...)
      x
      v)
-  (v (lambda (x) e)
-     binop (binop v)
-     unop
-     number)
-  (binop + - *)
-  (unop zero?)
-  (x variable)
-  (pc ((store (x v) ...) ec))
-  (ec (ec e)
-      (v ec)
-      (set! variable ec)
-      (let ((x ec)) e)
-      (if ec e e)
-      (begin ec e e ...)
-      hole))
+  (v (λ (x_!_ ...) e)
+     + - * =
+     number
+     (void)
+     #t
+     #f)
+  (x variable-not-otherwise-mentioned)
+
+  #:binding-forms
+  (λ (x ...) e #:refers-to (shadow x ...))
+  (let ([x e_x] ...) e_body #:refers-to (shadow x ...))
+  (letrec ([x e_x] ...) #:refers-to (shadow x ...) e_body #:refers-to (shadow x ...)))
+
+(define-extended-language lang surface-lang
+  (p ((store (x_!_ v-or-undefined) ...) e))
+  (e ::= ....
+     undefined
+     (lset! x e))
+  
+  (P ((store (x v-or-undefined) ...) E))
+  (E (v ... E e ...)
+     (set! x E)
+     (lset! x E)
+     (let ((x v-or-undefined) ... (x E) (x e) ...) e)
+     (if E e e)
+     (begin E e e ...)
+     hole)
+  
+  (v-or-undefined v undefined))
+
 
 ;; collect : term -> term
 ;; performs a garbage collection on the term `p'
 (define (collect p)
-  (define (substitute var exp body)
-    (term-let ((var var) 
-               (exp exp)
-               (body body))
-      (term (subst (var exp body)))))
-  
+
   (define (find-unused vars p)
     (filter (λ (var) (unused? var p))
             vars))
-  
+
   (define (unused? var p)
     (let ([rhss (map cadr (cdar p))]
           [body (cadr p)])
-      (and (not (free-in? var body))
-           (andmap (λ (rhs) (not (free-in? var rhs)))
+      (and (not (in? var body))
+           (andmap (λ (rhs) (not (in? var rhs)))
                    rhss))))
-  
-  (define (free-in? var body)
-    (not (equal? (substitute var (gensym) body)
-                 body)))
-  
+
+  (define (in? var body)
+    (let loop ([body body])
+      (match body
+        [(cons a b) (or (loop a) (loop b))]
+        [(? symbol?) (equal? body var)]
+        [else #f])))
+
   (define (remove-unused vars p)
-    `((store ,@(filter (λ (binding) (not (memq (car binding) vars)))
-                       (cdar p)))
-      ,(cadr p)))
-  
-  (let* ([vars (map car (cdar p))]
-         [unused (find-unused vars p)])
-    (cond
-      [(null? unused) p]
-      [else
-       (collect (remove-unused unused p))])))
+    (term ((store ,@(filter (λ (binding) (not (memq (car binding) vars)))
+                            (cdar p)))
+           ,(cadr p))))
+
+  (define vars
+    (match p
+      [`((store (,xs ,vs) ...) ,e) xs]))
+  (define unused (find-unused vars p))
+  (cond
+    [(null? unused) p]
+    [else
+     (collect (remove-unused unused p))]))
+
+(module+ test
+  (test-equal (collect (term ((store) 1))) (term ((store) 1)))
+  (test-equal (collect (term ((store (x 1)) 1))) (term ((store) 1)))
+  (test-equal (collect (term ((store (x 1)) x))) (term ((store (x 1)) x)))
+  (test-equal (collect (term ((store (x 1) (y x)) 1))) (term ((store) 1)))
+  (test-equal (collect (term ((store (x 1) (y x)) y))) (term ((store (x 1) (y x)) y)))
+  ;; doesn't really do actually gc, so improving the collect
+  ;; function might break this test (which would be great)
+  (test-equal (collect (term ((store (x y) (y x)) 1))) (term ((store (x y) (y x)) 1))))
 
 (define reductions
   (reduction-relation
-   lang
-   (==> (in-hole pc_1 (begin v e_1 e_2 ...))
-        (in-hole pc_1 (begin e_1 e_2 ...))
-        begin\ many)
-   
-   (==> (in-hole pc_1 (begin e_1))
-        (in-hole pc_1 e_1)
-        begin\ one)
-   
-   (==> ((store (x_before v_before) ...
-           (x_i v_i)
-           (x_after v_after) ...)
-         (in-hole ec_1 x_i))
-        ((store 
-             (x_before v_before) ... 
-           (x_i v_i)
-           (x_after v_after) ...)
-         (in-hole ec_1 v_i))
-        deref)
-   
-   (==> ((store (x_before v_before) ...
-           (x_i v)
-           (x_after v_after) ...)
-         (in-hole ec_1 (set! x_i v_new)))
-        ((store (x_before v_before) ... 
-           (x_i v_new)
-           (x_after v_after) ...)
-         (in-hole ec_1 v_new))
-        set!)     
-   
-   (==> (in-hole pc_1 ((lambda (x_1) e_1) v_1))
-        (in-hole pc_1 (subst (x_1 v_1 e_1)))
-        βv)
+   lang #:domain p
+   (==> (in-hole P_1 (begin v e_1 e_2 ...))
+        (in-hole P_1 (begin e_1 e_2 ...))
+        "begin many")
 
-   (==> (in-hole pc_1 ((binop v_1) v_2))
-        (in-hole pc_1
-                 ,((match (term binop) ['+ +] ['- -]['* *])
-                   (term v_1) (term v_2)))
-        δ-binop)
+   (==> (in-hole P_1 (begin e_1))
+        (in-hole P_1 e_1)
+        "begin one")
 
-   (==> (in-hole pc_1 (unop v_1))
-        (in-hole pc_1
-                 ,((match (term unop) ['zero? zero?])
-                   (term v_1)))
-        δ-unop)
+   (==> ((store (x_before v-or-undefined_before) ...
+                (x_i v_i)
+                (x_after v-or-undefined_after) ...)
+         (in-hole E_1 x_i))
+        ((store (x_before v-or-undefined_before) ...
+                (x_i v_i)
+                (x_after v-or-undefined_after) ...)
+         (in-hole E_1 v_i))
+        "get")
 
-   (==> (in-hole pc_1 (if v_test e_then e_else))
-        (in-hole pc_1 ,(if (not (zero? (term v_test)))
-                           (term e_then)
-                           (term e_else)))
-        if)
-   
-   (==> ((store (name the-store any) ...)
-         (in-hole ec_1 (let ((x_1 v_1)) e_1)))
-        ,(let ((new-x (variable-not-in (term (the-store ...)) (term x_1))))
-           (term
-            ((store the-store ... (,new-x v_1))
-             (in-hole ec_1 (subst (x_1 ,new-x e_1))))))
-        let)
-   
-   (==> (in-hole pc_1 (letrec ((x_1 e_1)) e_2))
-        (in-hole pc_1 (let ((x_1 0)) (begin (set! x_1 e_1) e_2)))
-        letrec)
-   
+   (==> ((store (x_before v-or-undefined_before) ...
+                (x_i v_old)
+                (x_after v-or-undefined_after) ...)
+         (in-hole E (set! x_i v_new)))
+        ((store (x_before v-or-undefined_before) ...
+                (x_i v_new)
+                (x_after v-or-undefined_after) ...)
+         (in-hole E (void)))
+        "set!")
+   (==> ((store (x_before v-or-undefined_before) ...
+                (x_i v-or-undefined)
+                (x_after v-or-undefined_after) ...)
+         (in-hole E (lset! x_i v_new)))
+        ((store (x_before v-or-undefined_before) ...
+                (x_i v_new)
+                (x_after v-or-undefined_after) ...)
+         (in-hole E (void)))
+        "lset!")
+
+   (==> (in-hole P ((λ (x ..._1) e) v ..._1))
+        (in-hole P (let ([x v] ...) e))
+        "βv")
+
+   (==> (in-hole P (= number_1 number_2 number_3 ...))
+        (in-hole P ,(apply = (term (number_1 number_2 number_3 ...))))
+        "=")
+   (==> (in-hole P (- number_1 number_2 ...))
+        (in-hole P ,(apply - (term (number_1 number_2 ...))))
+        "-")
+   (==> (in-hole P (+ number ...))
+        (in-hole P ,(apply + (term (number ...))))
+        "+")
+   (==> (in-hole P (* number ...))
+        (in-hole P ,(apply * (term (number ...))))
+        "*")
+   (==> (in-hole P (zero? number))
+        (in-hole P (δ zero? number))
+        "zero")
+
+   (==> (in-hole P (if #f e_then e_else))
+        (in-hole P e_else)
+        "iff")
+   (==> (in-hole P (if v e_then e_else))
+        (in-hole P e_then)
+        (side-condition (not (equal? (term v) #f)))
+        "ift")
+
+   (==> ((store (x_old v-or-undefined_old) ...)
+         (in-hole E (let ([x_1 v-or-undefined_1] [x_2 v-or-undefined_2] ...) e)))
+        ((store (x_old v-or-undefined_old) ... (x_new v-or-undefined_1))
+         (in-hole E (let ([x_2 v-or-undefined_2] ...) (substitute e x_1 x_new))))
+        (fresh x_new)
+        "let1")
+   (==> (in-hole P (let () e))
+        (in-hole P e)
+        "let0")
+
+   (==> (in-hole P (letrec ((x e_1) ...) e_2))
+        (in-hole P (let ((x undefined) ...) (begin (lset! x e_1) ... e_2)))
+        "letrec")
+
    with
    [(--> a ,(collect (term b))) (==> a b)]))
 
-
-
-(define (run e) (traces reductions `((store) ,e)))
-
-(define (show-some-reductions)
-  (run '(letrec ((f (lambda (x)
-                      (letrec ((y (f 1)))
-                        2))))
-          (f 3)))
-
-  (run '(letrec ((f (lambda (x)
-                      (letrec ((y 1))
-                        (f 1)))))
-          (f 3))))
+(define (run e) (traces reductions (term ((store) ,e))))
 
 (define (result-of prog)
-  (match (apply-reduction-relation* reductions `((store) ,prog))
+  (match (apply-reduction-relation* reductions (term ((store) ,prog)))
     [`(((store . ,_) ,res)) res]))
 
+(module+ test
+  (test-equal
+   (result-of (term (let ((x 5))
+                      (begin
+                        (set! x 6)
+                        x))))
+   6)
 
-(time
- (test-equal
-  (result-of `(let ((x 5))
-                (begin
-                  (set! x 6)
-                  x)))
-  6)
+  (test-equal
+   (result-of
+    (term (letrec ((f (λ (x) (begin (set! f x) f))))
+            (begin (f 8)
+                   f))))
+   8)
 
- (test-equal
-  (result-of
-   `(letrec ((f (lambda (x) (begin (set! f x) f))))
-      (begin (f 8)
-             f)))
-  8)
+  (test-equal (result-of (term (+ 1 2 3 4))) 10)
+  (test-equal (result-of (term (- 1 2 3 4))) -8)
+  (test-equal (result-of (term (* 1 2 3 4))) 24)
+  (test-equal (result-of (term (if (= 0 0) 1 2))) 1)
+  (test-equal (result-of (term (if (= 0 2) 1 2))) 2)
+  (test-equal (result-of (term (letrec ([x 1][y 2]) (+ x y)))) 3)
+  (test-equal (result-of (term (letrec ([x 1][y x][z 3]) y))) 1)
 
- ;; changing `xx` to `x` produces an error, but it shouldn't
+  ;; test it gets stuck
+  (test-equal (redex-match lang
+                           v
+                           (result-of (term (letrec ((B (set! B #t))) 1))))
+              #f)
 
- (test-equal
-  (result-of
-   `(let ((x 9999))
-      (let ((x 5))
-        (let ((double (lambda (x) (let ((xx x))
-                                    (begin (set! xx ((+ xx) xx)) xx)))))
-          ((+ (double ((+ (double (double x))) x))) (double x))))))
-  60)
+  (test-equal
+   (result-of
+    (term (let ((x 9999))
+            (let ((x 5))
+              (let ((double (λ (x) (let ((xx x))
+                                     (begin (set! xx (+ xx xx)) xx)))))
+                (+ (double x)
+                   (+ (double (double x))
+                      x)
+                   (double x)))))))
+   45)
 
- (test-equal
-  (result-of
-   `(letrec ((fact
-              (lambda (x)
-                (if x
-                    ((* x) (fact ((- x) 1)))
-                    1))))
-      (fact 5)))
-  120)
+  (test-equal
+   (result-of
+    (term (letrec ((fact
+                    (λ (x)
+                      (if (= x 0)
+                          1
+                          (* x (fact (- x 1)))))))
+            (fact 5))))
+   120)
 
- (test-equal
-  (result-of
-   `(let ((even? 0))
-      (let ((odd? 0))
-        (begin
-          (set! even?
-                (lambda (x)
-                  (if x
-                      (odd? ((- x) 1))
-                      1)))
-          (set! odd?
-                (lambda (x)
-                  (if x
-                      (even? ((- x) 1))
-                      0)))
-          ((+
-            ((+ (odd? 17))
-             ((* (odd? 18)) 10)))
-           ((* (odd? 19)) 100))))))
-  101)
+  (test-equal
+   (result-of
+    (term (let ((even? 0))
+            (let ((odd? 0))
+              (begin
+                (set! even?
+                      (λ (x)
+                        (if (= x 0)
+                            1
+                            (odd? (- x 1)))))
+                (set! odd?
+                      (λ (x)
+                        (if (= x 0)
+                            0
+                            (even? (- x 1)))))
+                (+ (+ (odd? 1)
+                      (* (odd? 2) 10))
+                   (* (odd? 3) 100)))))))
+   101)
 
- (test-equal
-  (result-of
-   `(let ((n 5))
-      (let ((acc 1))
-        (letrec ((imperative-fact (lambda (ignored)
-                                    (if n
-                                        (begin
-                                          (set! acc ((* acc) n))
-                                          (set! n ((- n) 1))
-                                          (imperative-fact 9999))
-                                        acc))))
-          (imperative-fact 9999)))))
-  120))
-
-
-
-(test-results)
+  (test-equal
+   (result-of
+    (term (let ([n 5]
+                [acc 1])
+            (letrec ([imperative-fact
+                      (λ ()
+                        (if (= n 0)
+                            acc
+                            (begin
+                              (set! acc (* acc n))
+                              (set! n (- n 1))
+                              (imperative-fact))))])
+              (imperative-fact)))))
+   120))
