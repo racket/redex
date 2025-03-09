b38e1cf6fa064b1ab72814221b5a4136891763db
Robby Findler <robby@racket-lang.org>
2025-03-08 18:57:01 -0600

    adjust pict-returning functions to convert from racket picts to rhombus picts

--- redex-pict-lib/redex/private/rhombus-bridge.rkt
+++ redex-pict-lib/redex/private/rhombus-bridge.rkt
@@ -6,11 +6,11 @@
 // on rhombus by replacing this file with
 // racket code that does the same thing
 
-import:
-  pict open
-
-export:
-  find
+import: pict open
+export: find from_handle
 
 fun find(pict,sub,h,v):
   Find(sub, ~horiz: h, ~vert: v).in(pict)
+
+fun from_handle(p):
+  Pict.from_handle(p)
